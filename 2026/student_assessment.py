eaa5283791e1455564c66f899c032adb00805a60
z4m9 <samuelmarriott+github@gmail.com>
2026-02-21 18:39:31 +1100

    21feb

--- 2026/student_assessment.py
+++ 2026/student_assessment.py
@@ -5,15 +5,18 @@ def get_student_details():
     return name1, results, max_possible
 
 # Main
-student_name, score, possible_marks = get_student_details()
-print(f"Your name: {student_name}")
-percentage = score / possible_marks * 100
+test = True
+while test:
+    student_name, score, possible_marks = get_student_details()
+    print(f"Your name: {student_name}")
+    percentage = score / possible_marks * 100
+    percentage_rounded = round(percentage, 0)
 
-if percentage < 50 and percentage >= 0:
-    print("You failed the assessment, please re-do it.")
-    print(f"Your percentage score: {percentage}")
-elif percentage >= 50 and percentage <= 100:
-    print("Well done. You passed the assessment.")
-    print(f"Your percentage score: {percentage}")
-else:
-    print("Please enter a valid score.")
+    if percentage < 50 and percentage >= 0:
+        print("You failed the assessment, please re-do it.")
+        print(f"Your percentage score: {percentage_rounded}")
+    elif percentage >= 50 and percentage <= 100:
+        print("Well done. You passed the assessment.")
+        print(f"Your percentage score: {percentage_rounded}")
+    else:
+        print("Please enter a valid score.")

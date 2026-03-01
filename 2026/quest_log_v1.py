9fb93ee7be20b5b28533100fddfdd826db61f71e
z4m9 <samuelmarriott+github@gmail.com>
2026-03-01 14:59:48 +1100

    suggest_random_quest

--- 2026/quest_log_v1.py
+++ 2026/quest_log_v1.py
@@ -2,6 +2,8 @@
 # PF 5.4
 # Samuel Marriott 1/03/2026
 
+import random
+
 def show_menu():
     #print("show_menu stub called")
     print("\n Quests Log Menu")
@@ -11,7 +13,8 @@ def show_menu():
     print("3: Completed quests")
     print("4: Sort quests A-Z")
     print("5: Sort quests Z-A")
-    print("6: Quit")
+    print("6: Suggest random quest")
+    print("7: Quit")
 
 def add_quest(quests):
     #print("add_quest stub") # Temporary
@@ -25,17 +28,37 @@ def list_quests(quests):
     if quests == []:
         print("No quests yet")
     else:
+        i = 0
         for quest in quests:
-            print(quest)
+            print(i,". ", quest)
+            i += 1
 
 def complete_quest(quests):
-    print("complete_quests stub")
+    #print("complete_quests stub")
+    list_quests(quests)
+    item = int(input("Which quest has been completed and can be removed? "))
+    quests.pop(item)
+    print(f"Quest confirmed removed: {item}")
+    list_quests(quests)
 
 def sort_quests_az(quests):
-    print("sort_quest_az (A-Z) stub")
+    #print("sort_quest_az (A-Z) stub")
+    quests.sort()
+    print(quests)
+    print("List sorted alphabetically confirmed")
 
 def sort_quests_za(quests):
-    print("sort_quests_za (Z-A) stub")
+    #print("sort_quests_za (Z-A) stub")
+    quests.sort(reverse = True)
+    print(quests)
+    print("List was reverse sorted.")
+
+def suggest_random_quest(quests):
+    if len(quests) == []:
+        print("No quests to choose from.")
+    else:
+        random_quest = random.choice(quests)
+        print(random_quest)
 
 def main():
     # TODO: Create an empty list called quests
@@ -58,10 +81,11 @@ def main():
         elif option == "5":
             sort_quests_za(quests)
         elif option == "6":
+            suggest_random_quest(quests)
+        elif option == "7":
             print("👋 Goodbye!")
             break
         else:
             print("⚠️ Invalid option. Please choose 1-6.")
-
 
 main()
